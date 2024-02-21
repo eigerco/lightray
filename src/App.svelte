8f041100f568563ae1a874b14e8c8439b297d6f7
daniel.vladco <vladcodaniel@gmail.com>
2024-02-21 16:56:02 +0200

    feat: add Clear database button

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,14 @@
 <script>
     import {onMount} from 'svelte';
     import {writable} from 'svelte/store';
+    import {fade} from 'svelte/transition';
 
     let isConnected = writable(false);
     let isStarted = writable(false);
     let isModuleLoaded = writable(false); // State to track if the module is loaded
     let bootstrapAddresses = "";
     let peerID = '';
+    let notification = null
 
     function appendLog(msg, type = 'info') {
         let wasmLogs = document.getElementById("wasm_logs");
@@ -70,10 +72,21 @@
         }
     }
 
-    function initNode() {
-        if (window.initNode && typeof window.initNode === 'function') {
-            window.initNode(bootstrapAddresses)
-        }
+    function clearDatabase() {
+        let request = indexedDB.deleteDatabase("celestia");
+
+        request.onerror = (e) => createNotification("error", "Error deleting database.")
+        request.onblocked = (e) => createNotification("error", "Couldn't delete database due to the operation being blocked.")
+        request.onsuccess = (e) => createNotification("success", "Database deleted successfully.")
+    }
+
+    function createNotification(type, message) {
+        notification = { type, message }
+        setTimeout(closeNotification, 5000)
+    }
+
+    function closeNotification() {
+        notification = null
     }
 
     function startedNode() {
@@ -117,10 +130,10 @@
         try {
 
             // Local demo requires following.
-            //const response = await fetch('http://localhost:8096/bootstrap-peers');
+            const response = await fetch('http://localhost:8096/bootstrap-peers');
 
             // Production demo requires following.
-            const response = await fetch('/bootstrap-peers');
+            // const response = await fetch('/bootstrap-peers');
 
             if (response.ok) {
                 const data = await response.json();
@@ -135,6 +148,41 @@
 </script>
 
 <main>
+    {#if notification }
+    <div transition:fade={{ delay: 250, duration: 1000 }} class="absolute right-10 top-10 flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow-lg" role="alert">
+        {#if notification.type === 'success'}
+            <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
+                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+                </svg>
+                <span class="sr-only">Check icon</span>
+            </div>
+        {:else if notification.type === 'warning'}
+            <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-orange-500 bg-orange-100 rounded-lg dark:bg-orange-700 dark:text-orange-200">
+                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM10 15a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm1-4a1 1 0 0 1-2 0V6a1 1 0 0 1 2 0v5Z"/>
+                </svg>
+                <span class="sr-only">Warning icon</span>
+            </div>
+        {:else if notification.type === 'error'}
+            <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg dark:bg-red-800 dark:text-red-200">
+                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z"/>
+                </svg>
+                <span class="sr-only">Error icon</span>
+            </div>
+        {/if}
+        <div class="ms-3 text-sm font-normal">{notification.message}</div>
+        <button
+                class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8"
+                type="button" aria-label="Close" on:click={closeNotification}>
+            <span class="sr-only">Close</span>
+            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+            </svg>
+        </button>
+    </div>
+    {/if}
     <div class="container mx-auto p-8">
         <h1 class="text-3xl font-bold mb-6">LightRay - Celestia Go Wasm Light Node</h1>
         <p class="text-lg mb-6">LightRay enables you to:</p>
@@ -157,8 +205,6 @@
                         class:hover:bg-gray-700={(!$isModuleLoaded)}
                         class:bg-blue-500={($isModuleLoaded)}
                         class:hover:bg-blue-700={($isModuleLoaded)}
-                        class:bg-red-500={(!$isModuleLoaded && !$isConnected)}
-                        class:hover:bg-red-700={(!$isModuleLoaded && !$isConnected)}
                         class="text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                         on:click="{connectToNode}" disabled={!$isModuleLoaded}>
                     {#if $isModuleLoaded}
@@ -170,6 +216,10 @@
                     {/if}
                 </button>
             {/if}
+
+            <button on:click="{clearDatabase}" class="text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline bg-red-500 hover:bg-red-700">
+                <span style="color: white;">Clear database</span>
+            </button>
         </div>
 
         <div class="bg-white  pt-6 mt-2">
